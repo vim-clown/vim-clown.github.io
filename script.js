14686b0ddcc7e36ccfcb33f91e7f3a81d75622d4
vim-clown <86880403+vim-clown@users.noreply.github.com>
2022-12-02 21:24:47 +0300

    🎭

---
+++ script.js
@@ -0,0 +1,8 @@
+function showButtons() {
+    var buttons = document.querySelector('.buttons');
+    if (buttons.style.display === 'none') {
+      buttons.style.display = 'block';
+    } else {
+      buttons.style.display = 'none';
+    }
+  }
