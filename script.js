264492bfb17f312c33281f463b3e97c6fe4f2b44
vim-clown <86880403+vim-clown@users.noreply.github.com>
2022-12-03 20:35:41 +0300

    🎭

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function showButtons() {
+function showButtons(icon) {
     var buttons = document.querySelector('.buttons');
     if (buttons.style.display === 'none') {
+      // Rotate the image by 90 degrees
+      icon.classList.add("rotated");
+      icon.style.transform = "rotate(90deg)";
       buttons.style.display = 'block';
     } else {
       buttons.style.display = 'none';
+      // Rotate the image back to its original orientation
+      icon.classList.remove("rotated");
+      icon.style.transform = "rotate(0deg)";
     }
-  }
+}
