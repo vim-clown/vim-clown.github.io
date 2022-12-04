f8d04808579061647ecb538e26d02d5b73d1354d
vim-clown <86880403+vim-clown@users.noreply.github.com>
2022-12-04 18:42:47 +0300

    🎭

--- script.js
+++ script.js
@@ -1,14 +1,73 @@
 function showButtons(icon) {
     var buttons = document.querySelector('.buttons');
+    var title = document.querySelector('.title2');
+    var root = document.querySelector('.title');
+    var titleb = document.querySelector('.titleb');
+
+    var buttons1 = document.querySelector('.buttons2');
+    var title1 = document.querySelector('.title3');
+
+    var ic = document.querySelector('.icon3');
+
     if (buttons.style.display === 'none') {
-      // Rotate the image by 90 degrees
       icon.classList.add("rotated");
       icon.style.transform = "rotate(90deg)";
       buttons.style.display = 'grid';
+      title.style.display = 'none'
+      root.style.display = 'none'
+      titleb.style.display = 'grid'
+
+      buttons1.style.display = 'none';
+      title1.style.display = 'none';
+      ic.style.display = 'none' 
     } else {
       buttons.style.display = 'none';
-      // Rotate the image back to its original orientation
+      title.style.display = 'grid'
       icon.classList.remove("rotated");
       icon.style.transform = "rotate(0deg)";
+      titleb.style.display = 'none'
+      root.style.display = 'grid'
+
+      buttons1.style.display = 'none';
+      title1.style.display = 'grid';
+      ic.style.display = 'grid'
     }
+}
+
+function showEmpty(icon) {
+  var buttons = document.querySelector('.buttons2');
+  var title = document.querySelector('.title3');
+  var root = document.querySelector('.title');
+  var titlec = document.querySelector('.titlec');
+
+  var buttons1 = document.querySelector('.buttons');
+  var title1 = document.querySelector('.title2');
+
+  var ic = document.querySelector('.icon');
+
+  if (buttons.style.display === 'none') {
+    icon.classList.add("rotated");
+    icon.style.transform = "rotate(90deg)";
+    buttons.style.display = 'grid';
+    title.style.display = 'none';
+    root.style.display = 'none';
+    titlec.style.display = 'grid';
+
+    buttons1.style.display = 'none';
+    title1.style.display = 'none';
+    ic.style.display = 'none' 
+
+
+  } else {
+    buttons.style.display = 'none';
+    title.style.display = 'grid';
+    icon.classList.remove("rotated");
+    icon.style.transform = "rotate(0deg)";
+    titlec.style.display = 'none';
+    root.style.display = 'grid';
+
+    buttons1.style.display = 'none';
+    title1.style.display = 'grid';
+    ic.style.display = 'grid'
+  }
 }
