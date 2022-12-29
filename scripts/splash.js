09c4270a41fa6d2a97c771fe4f416628a10df465
vim-clown <86880403+vim-clown@users.noreply.github.com>
2022-12-29 03:49:12 +0300

    ::

--- scripts/splash.js
+++ scripts/splash.js
@@ -1,6 +1,6 @@
 var splash = document.querySelector('.overlay')
 
 window.onload = function () {
-  document.title = 'Deskflop';
+  document.title = 'Deskflop 1.0';
   splash.style.display = 'none';
 };
